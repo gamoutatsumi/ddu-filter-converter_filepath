ee0e433512752a7ddcc409c5d1d050ea10a8d878
gamoutatsumi <tatsumi@gamou.dev>
2024-08-01 11:22:10 +0900

    use jsr

--- denops/@ddu-filters/converter_relativepath.ts
+++ denops/@ddu-filters/converter_relativepath.ts
@@ -2,14 +2,11 @@ import {
   BaseFilter,
   BaseFilterParams,
   DduItem,
-} from "https://deno.land/x/ddu_vim@v3.10.2/types.ts";
-import { fn } from "https://deno.land/x/ddu_vim@v3.10.2/deps.ts";
-import {
-  isAbsolute,
-  relative,
-} from "https://deno.land/std@0.214.0/path/mod.ts";
-import { is } from "https://deno.land/x/unknownutil@v3.15.0/mod.ts";
-import { FilterArguments } from "https://deno.land/x/ddu_vim@v3.10.2/base/filter.ts";
+} from "jsr:@shougo/ddu-vim@5.0.0/types";
+import * as fn from "jsr:@denops/std@7.0.1/function";
+import { isAbsolute, relative } from "jsr:@std/path@1.0.2";
+import { is } from "jsr:@core/unknownutil@3.18.1";
+import { FilterArguments } from "jsr:@shougo/ddu-vim@5.0.0/filter";
 
 function getPath(item: DduItem): string | undefined {
   if (is.ObjectOf({ action: is.ObjectOf({ path: is.String }) })(item)) {
@@ -18,30 +15,33 @@ function getPath(item: DduItem): string | undefined {
 }
 
 export class Filter extends BaseFilter<BaseFilterParams> {
-  async filter(
-    { items, denops }: FilterArguments<BaseFilterParams>,
-  ): Promise<DduItem[]> {
+  async filter({
+    items,
+    denops,
+  }: FilterArguments<BaseFilterParams>): Promise<DduItem[]> {
     const cwd = await fn.getcwd(denops);
-    return Promise.resolve(items.map((item) => {
-      const path = getPath(item);
-      if (path === undefined || !isAbsolute(path)) {
+    return Promise.resolve(
+      items.map((item) => {
+        const path = getPath(item);
+        if (path === undefined || !isAbsolute(path)) {
+          return item;
+        }
+        const { word, display = word, matcherKey } = item;
+        if (display !== path) {
+          return item;
+        }
+
+        const relPath = relative(cwd, display);
+
+        if (matcherKey === path) {
+          item.matcherKey = relPath;
+        }
+        item.word = relPath;
+        item.display = relPath;
+
         return item;
-      }
-      const { word, display = word, matcherKey } = item;
-      if (display !== path) {
-        return item;
-      }
-
-      const relPath = relative(cwd, display);
-
-      if (matcherKey === path) {
-        item.matcherKey = relPath;
-      }
-      item.word = relPath;
-      item.display = relPath;
-
-      return item;
-    }));
+      }),
+    );
   }
   params() {
     return {};
